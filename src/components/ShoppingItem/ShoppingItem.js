fd4fab01ff18b2f9f3407284033f99964ac88908
Luiz Kornrumpf <luiz.kornrumpf@gmail.com>
2022-09-28 12:08:40 +0200

    second challenge Shopping Cart

--- src/components/ShoppingItem/ShoppingItem.js
+++ src/components/ShoppingItem/ShoppingItem.js
@@ -1,7 +1,8 @@
+import {useState} from "react";
 import "./ShoppingItem.css";
 
 function ShoppingItem({ name, price }) {
-  const amount = 1;
+  const [amount,setAmount]=useState(0);
 
   return (
     <section className="item">
@@ -9,10 +10,10 @@ function ShoppingItem({ name, price }) {
       <p>price: {price}</p>
       <p>amount: {amount}</p>
       <p>total: {price * amount}</p>
-      <button className="item__button" type="button">
+      <button className="item__button" type="button" onClick={()=>setAmount(amount-1)}>
         -
       </button>
-      <button className="item__button" type="button">
+      <button className="item__button" type="button"onClick={()=>setAmount(amount+1)}>
         +
       </button>
     </section>
